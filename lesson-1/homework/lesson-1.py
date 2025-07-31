a828abf936a46a4e1bc3c42e36c820b3c1a9b446
umirzoqovjamshid143 <umirzoqovjamshid143@gmail.com>
2025-07-31 11:26:12 +0300

    Create lesson-1.py

---
+++ lesson-1/homework/lesson-1.py
@@ -0,0 +1,37 @@
+1 Given a side of square. Find its perimeter and area.
+a = 6
+
+perimetr = 4 * a
+
+yuza = a * a
+
+print("Kvadratning perimetri:", perimetr)
+print("Kvadratning yuzasi:", yuza)
+
+2 Given diameter of circle. Find its length.
+import math
+d = 5
+uzunlik = math.pi * d
+print("Aylananing uzunligi:", uzunlik)
+
+3 Given two numbers a and b. Find their mean.
+a = 8
+b = 12
+orta_arifmetik = (a + b) / 2
+print("O'rta arifmetik:", orta_arifmetik)
+
+4 Given two numbers a and b. Find their sum, product and square of each number.
+a = 5
+b = 3
+
+yigindi = a + b
+print("Yig'indisi:", yigindi)
+
+kopaytma = a * b
+print("Ko‘paytmasi:", kopaytma)
+
+a_kvadrat = a ** 2
+print("a ning kvadrati:", a_kvadrat)
+b_kvadrat = b ** 2
+print("b ning kvadrati:", b_kvadrat)
+
